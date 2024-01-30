64660f4428c1a6b571b2a603d01f2ee37f788757
Nestor Masinde <nestormasinde@gmail.com>
2024-01-30 21:48:14 +0300

    JSX_lab successful!

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (<div id="about">
+    <h2>About Me</h2>
+    <p>If it's easy, do it easy.If it's hard, do it hard</p>
+    <img src={image} alt="I made this"/>
+    </div>)
 }
 
 export default About;
